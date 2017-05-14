2c7c8cc79afc620710033ba0f1b3e45f9edb2c7d
Sven Wick <swick@thinkcentre.gnu.local>
2017-05-14 23:04:54 +0200

    initial sphinx documentation

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to gnome-shell-extension-examples&#8217;s documentation!"],titleterms:{document:0,exampl:0,extens:0,gnome:0,indice:0,shell:0,tabl:0,welcom:0}})
